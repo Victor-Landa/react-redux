b4d371680db785bdac9eb9f6f262aa5e245ca9a0
Victor Landa <viiicktor19@gmail.com>
2019-06-21 02:55:25 -0500

    estilos al td

--- src/components/App.js
+++ src/components/App.js
@@ -2,46 +2,50 @@ import React from 'react';
 
 const App = () => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>
-            Nombre
-          </th>
-          <th>
-            Correo
-          </th>
-          <th>
-            Enlace
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            Víctor
-          </td>
-          <td>
-            victorlanda1915@gmail.com
-          </td>
-          <td>
-            https://github.com/Victor-Landa
-          </td>
-        </tr>
-        <tr>
-          <td>
-            Paola
-          </td>
-          <td>
-            paola_huerta@gmail.com
-          </td>
-          <td>
-            https://paolahuerta.com
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="margen">
+      <table className="tabla">
+        <thead>
+          <tr>
+            <th>
+              Nombre
+            </th>
+            <th>
+              Correo
+            </th>
+            <th>
+              Enlace
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Víctor
+            </td>
+            <td>
+              victorlanda1915@gmail.com
+            </td>
+            <td>
+              https://github.com/Victor-Landa
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Paola
+            </td>
+            <td>
+              paola_huerta@gmail.com
+            </td>
+            <td>
+              https://paolahuerta.com
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
 export default App;
+
+
