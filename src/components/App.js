f64d7ed5feba922d04d769457840e1beba66b255
Victor Landa <viiicktor19@gmail.com>
2019-06-21 13:18:00 -0500

    Importé Menu en App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Menu from './Menu';
 
 const App = () => (
   <BrowserRouter>
-    
+    <Menu/>
   </BrowserRouter>
 )
 
