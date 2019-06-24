5026ba4b49d43a3c7f31680e467a11bb58de410e
Victor Landa <viiicktor19@gmail.com>
2019-06-24 17:38:11 -0500

    Componente Spinner

--- src/components/usuarios/index.js
+++ src/components/usuarios/index.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Spinner from '../general/Spinner';
+
 import * as usuariosActions from '../../actions/usuariosActions';
 
 class Usuarios extends Component {
 
    componentDidMount(){
     this.props.traerTodos();
+  }
+
+  ponerContenido = () => {
+
+    if(this.props.cargando){
+      return <Spinner />
+    }
+
+    return (
+      <table className="tabla">
+        <thead>
+          <tr>
+            <th>
+              Nombre
+            </th>
+            <th>
+              Correo
+            </th>
+            <th>
+              Enlace
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          { this.ponerFilas() }
+        </tbody>
+      </table>
+    )
   }
 
   ponerFilas = () => (
@@ -28,24 +58,7 @@ class Usuarios extends Component {
   render(){
     return (
       <div>
-        <table className="tabla">
-          <thead>
-            <tr>
-              <th>
-                Nombre
-              </th>
-              <th>
-                Correo
-              </th>
-              <th>
-                Enlace
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            { this.ponerFilas() }
-          </tbody>
-        </table>
+        { this.ponerContenido() }
       </div>
     );
   }
