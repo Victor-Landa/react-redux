40ac0e2c410f9eabb6db56cd80b6ac5513cbf6e3
Victor Landa <viiicktor19@gmail.com>
2019-06-24 00:13:35 -0500

    Los tres escenarios asíncronos

--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
-import { TRAER_TODOS } from '../types/usuariosTypes';
+import { TRAER_TODOS, CARGANDO, ERROR } from '../types/usuariosTypes';
 
 export const traerTodos = () => async (dispatch) => {
+
+    dispatch({
+        type: CARGANDO
+    })
 
     try{
         const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users')
@@ -12,6 +16,10 @@ export const traerTodos = () => async (dispatch) => {
         })
     } catch(error){
         console.log('Woops! ', error.message)
+        dispatch({
+            type: ERROR,
+            payload: error.message
+        })
     }
     
 }
