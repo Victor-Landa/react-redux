ac00a38580f8999c88ff16434f1d8a9597502b4b
Victor Landa <viiicktor19@gmail.com>
2019-06-24 19:53:14 -0500

    Múltiples Reducers

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as usuariosActions from '../../actions/usuariosActions';
+import * as publicacionesActions from '../../actions/publicacionesActions';
 
 class Publicaciones extends Component{
+
+    componentDidMount(){
+        if(!this.props.usuariosReducer.usuarios.length){
+            this.props.traerTodos();
+        }
+    }
+
     render(){
         return(
             <div>
+                <h1>
+                    Publicaciones de 
+                </h1>
                 { this.props.match.params.key }
             </div>
         )
     }
 }
 
-export default Publicaciones;
+const mapStateToProps = ({ usuariosReducer, publicacionesReducer }) => {
+    return { usuariosReducer, publicacionesReducer };
+}
+
+const mapDispatchToProps = {
+    ...usuariosActions,
+    ...publicacionesActions
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Publicaciones);
