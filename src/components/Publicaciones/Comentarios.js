29de503caf1ba3224f2aff4f6ab2683dabb39bed
Victor Landa <viiicktor19@gmail.com>
2019-06-26 20:56:43 -0500

    Comentarios.js

---
+++ src/components/Publicaciones/Comentarios.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Spinner from '../general/Spinner';
+import Fatal from '../general/Fatal';
+
+
+const Comentarios = (props) => {
+    if(props.cargando){
+        return <Spinner />
+    }
+
+    if(props.error){
+        return <Fatal mensaje={ props.error } />
+    }
+
+    const ponerComentarios = () => (
+        props.comentarios.map((comentario) => (
+            <li>
+                <b>
+                    <u>
+                        { comentario.email }
+                    </u>
+                </b>
+                <br />
+                { comentario.body }
+            </li>
+        ))
+    )
+
+    return (
+        <ul>
+            { ponerComentarios() }
+        </ul>
+    )
+}
+
+const mapStateToProps = ({ publicacionesReducer }) => publicacionesReducer;
+
+export default connect(mapStateToProps)(Comentarios);
