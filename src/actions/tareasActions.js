873868b0b4ceb72a1595362ae558c0751f9d8892
Victor Landa <viiicktor19@gmail.com>
2019-06-27 19:41:04 -0500

    Normalizar Datos

--- src/actions/tareasActions.js
+++ src/actions/tareasActions.js
@@ -8,11 +8,22 @@ export const traerTodas = () => async (dispatch) => {
     })
 
     try{
-        const respuesta = await axios.get('https://jsonplaceholder.typicode.com/todos')
+        const respuesta = await axios.get('https://jsonplaceholder.typicode.com/todos');
+        
+        const tareas = {};
+
+        respuesta.data.map((tar) => (
+            tareas[tar.userId] = {
+                ...tareas[tar.userId],
+                [tar.id]: {
+                    ...tar
+                }
+            }
+        ));
 
         dispatch({
             type: TRAER_TODAS,
-            payload: respuesta.data
+            payload: tareas
         })
     } catch(error){
         console.log('Woops! ', error.message)
